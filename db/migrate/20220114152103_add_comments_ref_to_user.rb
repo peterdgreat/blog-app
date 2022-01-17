075d9fbac4375c8f6911ed061194e0ed7a7f2988
Ajayi Peter <peterdgreat2016@gmail.com>
2022-01-18 00:30:41 +0100

    Database rehenerated

--- db/migrate/20220114152103_add_comments_ref_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddCommentsRefToUser < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :users, :comment, null: false, foreign_key: true
-  end
-end
--- db/migrate/20220114153615_add_address_to_post.rb
+++
@@ -1,6 +0,0 @@
-class AddAddressToPost < ActiveRecord::Migration[7.0]
-  def change
-    add_column :posts, :address, :string
-    add_index :posts, :address
-  end
-end
