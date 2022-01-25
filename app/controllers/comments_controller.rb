962753f71ae448525f54654777da07e620fea7b5
Ajayi Peter <peterdgreat2016@gmail.com>
2022-01-25 22:17:48 +0100

    Comment Added to post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,29 @@
-def new
-    @comment = Comment.new
-end
-def create
-    @comment = Comment.new(params.require(:comment).permit(:text, :title))
-    if @comment.save
-      flash[:success] = "Comment successfully created"
-      redirect_to @comment
-    else
-      flash[:error] = "Something went wrong"
-      render 'new'
+class CommentsController < ApplicationController
+  def new
+    comment = Comment.new
+    respond_to do |format|
+      format.html do
+        render :new, locals: { comment: comment }
+      end
     end
-end
+  end
 
+  def create
+    comment = Comment.new(params.require(:comment).permit(:text, :title))
+    post = Post.find(params[:post_id])
+    user = User.find(params[:user_id])
+    comment.author = user
+    comment.post = post
+    respond_to do |format|
+      format.html do
+        if comment.save
+          flash[:success] = 'comment saved successfully'
+          redirect_to user_post_url(user.id, post.id)
+        else
+          flash.now[:error] = 'Error: Comment could not be saved'
+          render :new, locals: { comment: comment }
+        end
+      end
+    end
+  end
+end
