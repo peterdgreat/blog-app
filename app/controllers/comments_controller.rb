56d08ea884d8fef01672a69687bef09ef55de1b1
Ajayi Peter <peterdgreat2016@gmail.com>
2022-01-26 10:30:02 +0100

    comment saved successfully

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,14 +7,14 @@ class CommentsController < ApplicationController
   end
 
   def create
-    comment = Comment.new(params.require(:comment).permit(:text, :title))
+    comment = Comment.new(comment_params)
     post = Post.find(params[:post_id])
-    user = User.find(params[:user_id])
+    user = current_user
     comment.author = user
     comment.post = post
     respond_to do |format|
       format.html do
-        if comment.save && post.update_attribute(:comments_counter, post.comments_counter + 1)
+        if comment.save
           flash[:success] = 'comment saved successfully'
           redirect_to user_post_url(user.id, post.id)
         else
@@ -23,5 +23,11 @@ class CommentsController < ApplicationController
         end
       end
     end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
   end
 end
