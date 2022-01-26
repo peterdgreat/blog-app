f02c673b714175580d6e05bea7de3bc9246b7ea7
Ajayi Peter <peterdgreat2016@gmail.com>
2022-01-26 22:45:38 +0100

    Added rspec test for likes

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,10 +1,9 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
-
+after_save :like_update_counter
   def like_update_counter
     post = Post.find(post_id)
     post.update_attribute(:likes_counter, post.likes_counter + 1)
-    post.save
   end
 end
